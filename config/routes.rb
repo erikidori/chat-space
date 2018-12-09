acf302439f54cb56f4abf6333fc24d850d945fce
eri <jta.e.m6@gmail.com>
2018-12-09 21:26:52 +0900

    グループ作成
    #What
    ・グループを作成できるようにした
    ・ルートパスをgroupsコントローラーのindexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 # devise_for :users
-    root 'messages#index'   #ルートパスの指定
- 	resources :messages
-    resources :groups
+    root 'groups#index'   #ルートパスの指定
+ 	
+    resources :groups , only: [:new, :create, :edit, :update] do
+    	resources :messages
+    end
     resources :users, only: [:edit, :update] 
-    resources :groups, only: [:new, :create, :edit, :update]
 end
