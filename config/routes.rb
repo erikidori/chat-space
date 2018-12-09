7af7a9073038892e1e078f057778092c116a9e24
eri <jta.e.m6@gmail.com>
2018-12-09 16:27:43 +0900

    ユーザー管理＆グループ作成
    #What
    ・ユーザーログイン機能作成
    ・グループ作成機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
 # devise_for :users
     root 'messages#index'   #ルートパスの指定
  	resources :messages
     resources :groups
-    resources :users, only: [:show] 
-
+    resources :users, only: [:edit, :update] 
+    resources :groups, only: [:new, :create, :edit, :update]
 end
