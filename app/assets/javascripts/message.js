092e2732427ccbf9d4b8c4e8774361e44473e28d
eri <jta.e.m6@gmail.com>
2018-12-15 21:27:55 +0900

    メッセージの非同期通信化
    #What
    ・メッセージ送信の非同期通信化
    ・メッセージの表示を降順に変更
    
    #Why
    ・メッセージ送受信のタイミングを合わせずに通信を行える
    →後に実装予定の、定期更新が実装されたら受信しながら送信も行える
    ・最新メッセージが上に来たほうが見やすいから

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){
+  function buildHTML(message){
+  	if ( message.image ) {
+  	  var html =
+  	  `<div class="main__message__box">
+  <div class="main__message__box__top">
+    <div class="main__message__box__top__name">
+      ${message.user_name}
+    </div>
+    <div class="main__message__box__top__time">
+      ${message.date}
+    </div>
+  </div>
+  <div class="main__message__box__text">
+      <p class="lower-message__content">
+        ${message.content}
+      </p>
+  </div>
+  <asset_path src=${message.image} >
+</div>`
+return html;
+} else {
+  var html =
+  `<div class="main__message__box">
+  <div class="main__message__box__top">
+    <div class="main__message__box__top__name">
+      ${message.user_name}
+    </div>
+    <div class="main__message__box__top__time">
+      ${message.date}
+    </div>
+  </div>
+  <div class="main__message__box__text">
+      <p class="lower-message__content">
+        ${message.content}
+      </p>
+  </div>
+  </div>`
+  return html;
+  };
+}
+
+
+  $('#new_message').on('submit',function() {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      tipe: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+      .done(function(data){
+      	var html = buildHTML(data);
+      	$('.messages').append(html);
+      	$('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      	$('form')[0].reset();
+      })
+        .fail(function(){
+        	alert('error');
+        });
+        return false;
+      });
+});
